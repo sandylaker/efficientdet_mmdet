1e807a8f1ca5787a10e14f9066c509ddb9339757
sandylaker <yawei.li@tum.de>
2021-03-17 08:17:58 +0000

    update configs and gitignore

--- configs/_base_/datasets/pascal_voc_0712.py
+++ configs/_base_/datasets/pascal_voc_0712.py
@@ -38,19 +38,19 @@ test_pipeline = [
 
 
 data = dict(
-    samples_per_gpu=16,
+    samples_per_gpu=24,
     workers_per_gpu=4,
     train=dict(
-        type='RepeatDataset',
-        times=3,
-        dataset=dict(
-            type=dataset_type,
-            ann_file=[
-                data_root + 'VOC2007/ImageSets/Main/trainval.txt',
-                data_root + 'VOC2012/ImageSets/Main/trainval.txt'
-            ],
-            img_prefix=[data_root + 'VOC2007/', data_root + 'VOC2012/'],
-            pipeline=train_pipeline)),
+    type='RepeatDataset',
+    times=3,
+    dataset=dict(
+        type=dataset_type,
+        ann_file=[
+            data_root + 'VOC2007/ImageSets/Main/trainval.txt',
+            data_root + 'VOC2012/ImageSets/Main/trainval.txt'
+        ],
+        img_prefix=[data_root + 'VOC2007/', data_root + 'VOC2012/'],
+        pipeline=train_pipeline)),
     val=dict(
         type=dataset_type,
         ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',

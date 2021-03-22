e5d5ee68fe1209c815816e456b25cd1e9f4f21fc
sandylaker <yawei.li@tum.de>
2021-03-22 16:29:00 +0000

    correct learning rate for ddp training coco

--- configs/d1_coco.py
+++ configs/d1_coco.py
@@ -1,7 +1,14 @@
 _base_ = ['_base_/datasets/coco_detection.py', '_base_/models/efficientdet.py']
 
+model = dict(
+    scale=1,    # Modify
+    backbone=dict(
+        n_classes=1000),
+    bbox_head=dict(
+        num_classes=80))
+
 # optimizer
-optimizer = dict(type='SGD', lr=0.96, momentum=0.9, weight_decay=4e-5)
+optimizer = dict(type='SGD', lr=0.18, momentum=0.9, weight_decay=4e-5)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
@@ -10,7 +17,7 @@ lr_config = dict(
     warmup_iters=1000,
     warmup_ratio=0.001,
     min_lr=1e-5)
-# actual epochs are multiplied by 3
+
 runner = dict(type='EpochBasedRunner', max_epochs=300)
 
 checkpoint_config = dict(interval=1, max_keep_ckpts=2)
